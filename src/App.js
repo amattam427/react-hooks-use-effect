8d0693e81fff031fab246a90a22df4f928d93ff8
Ashley Mattam <amattam427@gmail.com>
2022-01-19 03:58:21 -0500

    commit tree 066edbd8f41e115457e2fd268eaf102b2bdc2e5c

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  
+  //below we are telling react to update doc title when text variable changes.
+  useEffect(()=>{
+    document.title = text;
+  },[text])
+
+ //below we are telling react to reset the count variable back to 0 after 5 seconds.  
+  useEffect(()=>{
+    setTimeout(()=> setCount(0),5000);
+  }, [])
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={()=>setCount((count)=>count +1)}>I've been clicked {count} times</button>
+      <input
+        type='text'
+        placeholder='Type away...'
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+      />
+    </div>
+  )
 }
 
 export default App;
